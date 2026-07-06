e6ab8c66c6b88a43db9b118f73af33539dca7d4c
undergrounx <undergrounxs@gmail.com>
2026-07-07 01:40:58 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,13 @@
 // Firebase Console > Project Settings > General > Your apps > SDK setup
 // ============================================================
 const firebaseConfig = {
-  apiKey: "GANTI_DENGAN_API_KEY",
-  authDomain: "GANTI_DENGAN_AUTH_DOMAIN",
-  projectId: "GANTI_DENGAN_PROJECT_ID",
-  storageBucket: "GANTI_DENGAN_STORAGE_BUCKET",
-  messagingSenderId: "GANTI_DENGAN_SENDER_ID",
-  appId: "GANTI_DENGAN_APP_ID"
+  apiKey: "AIzaSyBZ63Yx1maOO-dkKwTjyOWXS3NLnvjIPjo",
+  authDomain: "avenor-30548.firebaseapp.com",
+  projectId: "avenor-30548",
+  storageBucket: "avenor-30548.firebasestorage.app",
+  messagingSenderId: "193865951588",
+  appId: "1:193865951588:web:f0176233e7a579547a7271",
+  measurementId: "G-V1XRCQY7ZH"
 };
 
 // Jangan ubah baris di bawah ini
